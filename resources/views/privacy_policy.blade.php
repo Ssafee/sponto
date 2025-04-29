4b3bc7190d824e1cc5653652c652c239f8902526
Safee <safeeullah.hussainy@irisdigi.com>
2025-04-29 18:27:13 +0500

    privacy policy

--- resources/views/privacy_policy.blade.php
+++ resources/views/privacy_policy.blade.php
@@ -1,3 +1,13 @@
+
+
+
+
+
+
+
+
+
+
 @extends('layouts.app')
 @section('content')
 
@@ -16,90 +26,95 @@
         </section>
         <!-- page-title end -->
 
-        <section class="service-section centred pt_110 pb_90" style="text-align: left">
+          <!-- service-section -->
+          <section class="service-section centred pt_110 pb_90" style="text-align: left">
             <div class="auto-container">
-               
+              
                 <div class="row clearfix">
                     <div class="col-lg-12 col-md-12 col-sm-12">
-                        <div class="content-box">
-                            <p><strong>Effective Date:</strong> 24th February, 2025<br>
-                            <strong>Company Name:</strong> Sponto LLC<br>
-                            <strong>Location:</strong> Casper, Wyoming, USA<br>
-                            <strong>Contact:</strong> <a href="mailto:compliance@sponto.co">compliance@sponto.co</a> | (813) 957-9908</p>
-                                <br>
-                                <br>
-                                <br>
-                            <ol>
-                                <li><strong>Who We Are</strong><br>
-                                    Sponto LLC (“Sponto,” “we,” “us,” or “our”) is a remote staffing company that connects U.S. and Canadian businesses with vetted offshore talent. We operate primarily from the U.S. and serve clients in North America.
-                                </li>
-                                <li><strong>Information We Collect</strong><br>
-                                    We collect the following information when you engage with our services:
-                                    <ul>
-                                        <li>Full Name</li>
-                                        <li>Email Address</li>
-                                        <li>Phone Number</li>
-                                        <li>Job Details</li>
-                                        <li>Resume</li>
-                                        <li>Loom Video (if submitted)</li>
-                                        <li>Company Information (e.g., name, location, nature of business)</li>
-                                    </ul>
-                                </li>
-                                <li><strong>How We Collect Information</strong><br>
-                                    Through:
-                                    <ul>
-                                        <li>Web Forms on our website</li>
-                                        <li>Direct communication (email, phone, SMS)</li>
-                                        <li>Uploaded files and video submissions</li>
-                                        <li>CRM systems like GoHighLevel and Excel</li>
-                                    </ul>
-                                </li>
-                                <li><strong>How We Use Your Information</strong><br>
-                                    To:
-                                    <ul>
-                                        <li>Understand your hiring needs</li>
-                                        <li>Deliver staffing services and talent placement</li>
-                                        <li>Communicate with you via phone, email, or SMS regarding your inquiry</li>
-                                        <li>Improve our internal processes and services</li>
-                                    </ul>
-                                    We may contact you using automated systems (including SMS or calls) where permitted. You may reply STOP to unsubscribe from SMS.
-                                    That messaging frequency may vary.
-                                </li>
-                                <li><strong>A2P Messaging & Consent</strong><br>
-                                    By submitting any of our forms, you consent to receive communications from Sponto related to your hiring needs. This includes:
-                                    <br><em>“I agree to be contacted by Sponto regarding my hiring needs and understand that I may receive emails, phone calls, and SMS messages related to my inquiry. Message and data rates may apply. You may reply STOP to unsubscribe.”</em><br>
-                                    We do not use ringless voicemails or pre-recorded voice messages.
-                                </li>
-                                <li><strong>Data Sharing and Selling</strong><br>
-                                    <ul>
-                                        <li>We do not sell your data to third parties.</li>
-                                        <li>We do not share your data for marketing or advertising purposes.</li>
-                                        <li>Your information is used solely for service delivery and internal business operations.</li>
-                                    </ul>
-                                </li>
-                                <li><strong>User Rights</strong><br>
-                                    You have the right to:
-                                    <ul>
-                                        <li>Request access to your data</li>
-                                        <li>Request correction or deletion</li>
-                                        <li>Opt-out of communications at any time</li>
-                                        <li>File a complaint by contacting <a href="mailto:compliance@sponto.co">compliance@sponto.co</a></li>
-                                    </ul>
-                                </li>
-                                <li><strong>Data Storage & Security</strong><br>
-                                    Your data is securely stored in systems like GoHighLevel and encrypted Excel files. We take reasonable steps to protect your information from unauthorized access.
-                                </li>
-                                <li><strong>Children's Privacy</strong><br>
-                                    Our services are not intended for individuals under the age of 18. We do not knowingly collect information from minors.
-                                </li>
-                                <li><strong>Policy Updates</strong><br>
-                                    We may update this policy from time to time. Changes will be posted with an updated effective date. Continued use of our services constitutes your acceptance of the updated policy.
-                                </li>
-                            </ol>
+                        <div class="service-block-one wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
+                            <div class="inner-boxs">
+                                <div class="lower-content">
+                                    <p><strong>Effective Date:</strong> 24th February, 2025<br>
+                                    <strong>Company Name:</strong> Sponto LLC<br>
+                                    <strong>Location:</strong> Casper, Wyoming, USA<br>
+                                    <strong>Contact:</strong> <a href="mailto:compliance@sponto.co">compliance@sponto.co</a> | (813) 957-9908</p>
+                                        <br>
+                                        <br>
+                                        <br>
+                                    <ol>
+                                        <li><strong>Who We Are</strong><br>
+                                            Sponto LLC (“Sponto,” “we,” “us,” or “our”) is a remote staffing company that connects U.S. and Canadian businesses with vetted offshore talent. We operate primarily from the U.S. and serve clients in North America.
+                                        </li>
+                                        <li><strong>Information We Collect</strong><br>
+                                            We collect the following information when you engage with our services:
+                                            <ul>
+                                                <li>Full Name</li>
+                                                <li>Email Address</li>
+                                                <li>Phone Number</li>
+                                                <li>Job Details</li>
+                                                <li>Resume</li>
+                                                <li>Loom Video (if submitted)</li>
+                                                <li>Company Information (e.g., name, location, nature of business)</li>
+                                            </ul>
+                                        </li>
+                                        <li><strong>How We Collect Information</strong><br>
+                                            Through:
+                                            <ul>
+                                                <li>Web Forms on our website</li>
+                                                <li>Direct communication (email, phone, SMS)</li>
+                                                <li>Uploaded files and video submissions</li>
+                                                <li>CRM systems like GoHighLevel and Excel</li>
+                                            </ul>
+                                        </li>
+                                        <li><strong>How We Use Your Information</strong><br>
+                                            To:
+                                            <ul>
+                                                <li>Understand your hiring needs</li>
+                                                <li>Deliver staffing services and talent placement</li>
+                                                <li>Communicate with you via phone, email, or SMS regarding your inquiry</li>
+                                                <li>Improve our internal processes and services</li>
+                                            </ul>
+                                            We may contact you using automated systems (including SMS or calls) where permitted. You may reply STOP to unsubscribe from SMS.
+                                            That messaging frequency may vary.
+                                        </li>
+                                        <li><strong>A2P Messaging & Consent</strong><br>
+                                            By submitting any of our forms, you consent to receive communications from Sponto related to your hiring needs. This includes:
+                                            <br><em>“I agree to be contacted by Sponto regarding my hiring needs and understand that I may receive emails, phone calls, and SMS messages related to my inquiry. Message and data rates may apply. You may reply STOP to unsubscribe.”</em><br>
+                                            We do not use ringless voicemails or pre-recorded voice messages.
+                                        </li>
+                                        <li><strong>Data Sharing and Selling</strong><br>
+                                            <ul>
+                                                <li>We do not sell your data to third parties.</li>
+                                                <li>We do not share your data for marketing or advertising purposes.</li>
+                                                <li>Your information is used solely for service delivery and internal business operations.</li>
+                                            </ul>
+                                        </li>
+                                        <li><strong>User Rights</strong><br>
+                                            You have the right to:
+                                            <ul>
+                                                <li>Request access to your data</li>
+                                                <li>Request correction or deletion</li>
+                                                <li>Opt-out of communications at any time</li>
+                                                <li>File a complaint by contacting <a href="mailto:compliance@sponto.co">compliance@sponto.co</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><strong>Data Storage & Security</strong><br>
+                                            Your data is securely stored in systems like GoHighLevel and encrypted Excel files. We take reasonable steps to protect your information from unauthorized access.
+                                        </li>
+                                        <li><strong>Children's Privacy</strong><br>
+                                            Our services are not intended for individuals under the age of 18. We do not knowingly collect information from minors.
+                                        </li>
+                                        <li><strong>Policy Updates</strong><br>
+                                            We may update this policy from time to time. Changes will be posted with an updated effective date. Continued use of our services constitutes your acceptance of the updated policy.
+                                        </li>
+                                    </ol>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>  
         </section>
         
       
